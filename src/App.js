cbbc27f891bbffaa38c8b214fc8b7e99a8ae2495
Giraffe_sungjun <kis07150@naver.com>
2023-09-15 20:25:04 +0900

    feat: 화면 애니메이션 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
 import Layout from './pages/Layout';
+import Welcome from './pages/Welcome';
 import "./styles/App.css"; // 스타일링 파일 임포트
 import Logo from './pages/Logo';
 
 function App() {
+  const [showLogo, setShowLogo] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowLogo(false);
+    }, 2000); // 2초 후 로고를 숨김
+  }, []);
+
+
   return (
     (
       <div>
+        {showLogo 
+        ? <Welcome /> 
+        : <div>
         <header className="header">
           <div className="header-content">
             <Logo />
@@ -20,6 +32,7 @@ function App() {
         </header>
         
         <Layout/>
+      </div>}
       </div>
     )
   );
