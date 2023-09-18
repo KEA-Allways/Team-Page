086d425ead9334feaf9a5272527751171e94d62b
Giraffe_sungjun <kis07150@naver.com>
2023-09-18 19:53:33 +0900

    Commit1 feat: Add memberPage
    Commit2
    refactor: refactoring App.js

---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Logo from "./Logo";
+import Layout from "./Layout";
+
+function Home(){
+    return(
+    <div>
+        <div>
+            <header className="header">
+                <div className="header-content">
+                    <Navbar className="header"/>
+                    <Logo />
+                    <h1>
+                        <span style={{ color: 'FBF562' }}>ALWAYS</span>{' '}
+                        <span style={{ color: '#43C0FF' }}>ALL WAYS</span> 
+                    </h1>
+                </div>
+            </header>
+        
+            <Layout/>
+        </div>     
+    </div>
+    )
+}
+
+export default Home;
