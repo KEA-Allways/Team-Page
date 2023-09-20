b724ee18893233c93c44bf889ba96307b47e51cc
Dajeong Choe <hers010801@gmail.com>
2023-09-20 13:16:55 +0900

    Design : 흰배경 깃허브 고양이 추가

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../styles/App.css'
 import logoImage from "../assets/logo.png"; 
-import GitIcon from "../assets/github.png";
+import GitIcon from "../assets/githubW.png";
 
 function Navbar() {
   return (
