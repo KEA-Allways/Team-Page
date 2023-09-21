243b82da6bdb8b479d4b1e165eb1b6209d401c96
Dajeong Choe <hers010801@gmail.com>
2023-09-21 21:16:03 +0900

    Design : 메뉴별 타이틀 텍스트 수정

--- src/pages/Vision.js
+++ src/pages/Vision.js
@@ -9,24 +9,23 @@ function Vision() {
       <Navbar />
       <motion.header className="header" initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
         <h1>
-          <span style={{ color: "#43C0FF", fontSize: "80px" }}>Always Developing</span>{" "}
-          <span style={{ color: "#FBF562", fontSize: "80px" }}>Finding All ways</span>
+          Always Developing Finding All ways
         </h1>
       </motion.header>
       <motion.div className="center" >
        
-        <motion.div className="card-center" initial={{ opacity: 0, scale: 0.5 }}
+        <motion.div className="vision-main" initial={{ opacity: 0, scale: 0.5 }}
               animate={{ opacity: 1, scale: 1 }}
               transition={{
                 duration: 1.0,
                 delay: 0.3,
                 ease: [0, 0.71, 0.2, 1.01]
               }}>
-          <p className="card-header-text">우리는 도전에 두려워 하지 않습니다.</p>
-          <p className="card-header-text">"가보지 않은 길은 위험하잖아?"</p>
-          <p className="card-header-text">배는 정박해 있을 때 가장 안전하지만 그것은 배의 존재 이유가 아닙니다.</p>
-          <p className="card-header-text">가보지 않은 길은 실패의 위험이 따르지만 우리는 혁신의 길을 가는 사람들입니다.</p>
-          <p className="card-header-text">'언제나-Always' 도전하고 '모든-All ways' 방법을 찾아내 더 나은 세상을 만들기 위해 노력합니다.</p>
+          <p className="vision-text">우리는 도전에 두려워 하지 않습니다.</p>
+          <p className="vision-text">"가보지 않은 길은 위험하잖아?"</p>
+          <p className="vision-text">배는 정박해 있을 때 가장 안전하지만 그것은 배의 존재 이유가 아닙니다.</p>
+          <p className="vision-text">가보지 않은 길은 실패의 위험이 따르지만 우리는 혁신의 길을 가는 사람들입니다.</p>
+          <p className="vision-text">'언제나-Always' 도전하고 '모든-All ways' 방법을 찾아내 더 나은 세상을 만들기 위해 노력합니다.</p>
         </motion.div>
       </motion.div>
       <motion.div className="card-container" initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }}>
